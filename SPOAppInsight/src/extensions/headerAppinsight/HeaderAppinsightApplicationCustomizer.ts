053405bb400243a4094994bf1f34ca0acf9e18e8
Frank Chen <chentao76@hotmail.com>
2021-03-08 10:53:27 -0800

    updated the sameple for latest AppInsight React lib

--- SPOAppInsight/src/extensions/headerAppinsight/HeaderAppinsightApplicationCustomizer.ts
+++ SPOAppInsight/src/extensions/headerAppinsight/HeaderAppinsightApplicationCustomizer.ts
@@ -4,9 +4,11 @@ import {
     BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
 import { Dialog } from '@microsoft/sp-dialog';
-import { autobind } from 'office-ui-fabric-react';
 
 import * as strings from 'HeaderAppinsightApplicationCustomizerStrings';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { createBrowserHistory } from 'history';
 
 const LOG_SOURCE: string = 'HeaderAppinsightApplicationCustomizer';
 
@@ -17,7 +19,7 @@ const LOG_SOURCE: string = 'HeaderAppinsightApplicationCustomizer';
  */
 export interface IHeaderAppinsightApplicationCustomizerProperties {
     // This is an example; replace with your own property
-    testMessage: string;
+    appInsightsKey: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
@@ -26,58 +28,44 @@ export default class HeaderAppinsightApplicationCustomizer
 
     @override
     public onInit(): Promise<void> {
-        Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
-
-        let message: string = this.properties.testMessage;
-        if (!message) {
-            message = '(No properties were provided.)';
-        }
-
-        // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
-
-        //this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
-        let html: string = `var sdkInstance="appInsightsSDK";window[sdkInstance]="appInsights";var aiName=window[sdkInstance],aisdk=window[aiName]||function(n){var o={config:n,initialize:!0},t=document,e=window,i="script";setTimeout(function(){var e=t.createElement(i);e.src=n.url||"https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js",t.getElementsByTagName(i)[0].parentNode.appendChild(e)});try{o.cookie=t.cookie}catch(e){}function a(n){o[n]=function(){var e=arguments;o.queue.push(function(){o[n].apply(o,e)})}}o.queue=[],o.version=2;for(var s=["Event","PageView","Exception","Trace","DependencyData","Metric","PageViewPerformance"];s.length;)a("track"+s.pop());var r="Track",c=r+"Page";a("start"+c),a("stop"+c);var u=r+"Event";if(a("start"+u),a("stop"+u),a("addTelemetryInitializer"),a("setAuthenticatedUserContext"),a("clearAuthenticatedUserContext"),a("flush"),o.SeverityLevel={Verbose:0,Information:1,Warning:2,Error:3,Critical:4},!(!0===n.disableExceptionTracking||n.extensionConfig&&n.extensionConfig.ApplicationInsightsAnalytics&&!0===n.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)){a("_"+(s="onerror"));var p=e[s];e[s]=function(e,n,t,i,a){var r=p&&p(e,n,t,i,a);return!0!==r&&o["_"+s]({message:e,url:n,lineNumber:t,columnNumber:i,error:a}),r},n.autoExceptionInstrumented=!0}return o}(
-            {
-              instrumentationKey:""
+        const browserHistory = createBrowserHistory({ window: window });
+        let appInsightKey: string = this.properties.appInsightsKey;
+        if (!appInsightKey || appInsightKey == "") {
+            const appkeyobj = require("../../../appinsightkey.json");
+            if (appkeyobj != null && appkeyobj["key"]) {
+                appInsightKey = appkeyobj["key"];
             }
-            );(window[aiName]=aisdk).queue&&0===aisdk.queue.length&&aisdk.trackPageView({});
-            `;
-        eval(html);
+        }
+        var reactPlugin = new ReactPlugin();
+        var appInsights = new ApplicationInsights({
+            config: {
+                maxBatchInterval: 0,
+                disableFetchTracking: true,
+                disableAjaxTracking: true,
+                enableAutoRouteTracking: true,
+                instrumentationKey: appInsightKey,
+                extensions: [reactPlugin],
+                extensionConfig: {
+                    [reactPlugin.identifier]: { history: browserHistory }
+                }
+            }
+        });
+        appInsights.loadAppInsights();
+        appInsights.trackPageView({
+            name: document.title, uri: window.location.href,
+            properties: {
+                ["CustomProps"]: {
+                    WebAbsUrl: this.context.pageContext.web.absoluteUrl,
+                    WebSerUrl: this.context.pageContext.web.serverRelativeUrl,
+                    WebId: this.context.pageContext.web.id,
+                    UserTitle: this.context.pageContext.user.displayName,
+                    UserEmail: this.context.pageContext.user.email,
+                    UserLoginName: this.context.pageContext.user.loginName
+                }
+            }
+        });
 
         return Promise.resolve();
     }
 
-    @autobind
-    private _renderPlaceHolders(): void {
-        let html: string = `var sdkInstance="appInsightsSDK";window[sdkInstance]="appInsights";var aiName=window[sdkInstance],aisdk=window[aiName]||function(n){var o={config:n,initialize:!0},t=document,e=window,i="script";setTimeout(function(){var e=t.createElement(i);e.src=n.url||"https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js",t.getElementsByTagName(i)[0].parentNode.appendChild(e)});try{o.cookie=t.cookie}catch(e){}function a(n){o[n]=function(){var e=arguments;o.queue.push(function(){o[n].apply(o,e)})}}o.queue=[],o.version=2;for(var s=["Event","PageView","Exception","Trace","DependencyData","Metric","PageViewPerformance"];s.length;)a("track"+s.pop());var r="Track",c=r+"Page";a("start"+c),a("stop"+c);var u=r+"Event";if(a("start"+u),a("stop"+u),a("addTelemetryInitializer"),a("setAuthenticatedUserContext"),a("clearAuthenticatedUserContext"),a("flush"),o.SeverityLevel={Verbose:0,Information:1,Warning:2,Error:3,Critical:4},!(!0===n.disableExceptionTracking||n.extensionConfig&&n.extensionConfig.ApplicationInsightsAnalytics&&!0===n.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)){a("_"+(s="onerror"));var p=e[s];e[s]=function(e,n,t,i,a){var r=p&&p(e,n,t,i,a);return!0!==r&&o["_"+s]({message:e,url:n,lineNumber:t,columnNumber:i,error:a}),r},n.autoExceptionInstrumented=!0}return o}(
-            {
-              instrumentationKey:""
-            }
-            );(window[aiName]=aisdk).queue&&0===aisdk.queue.length&&aisdk.trackPageView({});
-            `;
-
-
-        const html1 = `setInterval(function(){
-                    console.log("chen ok");
-                },1000);`;
-        let head: any = document.getElementsByTagName("head")[0] || document.documentElement,
-            script = document.createElement("script");
-        script.type = "text/javascript";
-
-        try {
-            // doesn't work on ie...
-            console.log('Append child');
-            script.appendChild(document.createTextNode(html));
-        }
-        catch (e) {
-            // IE has funky script nodes
-            console.log('Append child catch');
-            script.text = html;
-        }
-
-        console.log('Right before inserting');
-        head.insertBefore(script, head.firstChild);
-        console.log('Before executing');
-        //head.removeChild(script);
-    }
 }
